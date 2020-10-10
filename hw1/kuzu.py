a9048ce180fece5f21e9126e7484fd6072082580
Lyndon Chang <z5122528@student.unsw.edu.au>
2020-10-10 12:01:55 +1100

    Complete part 1.1

--- hw1/kuzu.py
+++ hw1/kuzu.py
@@ -10,19 +10,27 @@ class NetLin(nn.Module):
     # linear function followed by log_softmax
     def __init__(self):
         super(NetLin, self).__init__()
-        # INSERT CODE HERE
+        self.lin = nn.Linear(784,10)
 
     def forward(self, x):
-        return 0 # CHANGE CODE HERE
+        x = x.view(-1,784) # reshape to have 784 columns  
+        x = F.log_softmax(self.lin(x), dim=1)
+        return x 
 
 class NetFull(nn.Module):
     # two fully connected tanh layers followed by log softmax
     def __init__(self):
         super(NetFull, self).__init__()
-        # INSERT CODE HERE
+        self.hiddennodes = 90
+        self.h1 = nn.Linear(784, self.hiddennodes)
+        self.h2 = nn.Linear(self.hiddennodes, 10)
 
     def forward(self, x):
-        return 0 # CHANGE CODE HERE
+        x = x.view(-1,784)
+        x = torch.tanh(self.h1(x))
+        x = torch.tanh(self.h2(x))
+        x = F.log_softmax(x, dim=1)
+        return x
 
 class NetConv(nn.Module):
     # two convolutional layers and one fully connected layer,
